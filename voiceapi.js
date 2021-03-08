846b810ce6d5bc53b0c742ec1145a84d10bdcfc0
anands-vcloudx <anand.s@vcloudx.com>
2021-03-08 17:35:30 +0530

    aade method to invoke Voice API

--- voiceapi.js
+++ voiceapi.js
@@ -61,6 +61,189 @@ function ivrVoiceCall(callVoiceId, ttsPlayVoice, callback) {
   });
 }
 
+// Voice API to bridge the call
+function bridgeCall(callVoiceId, fromNumber , toNumber, callback) {
+  httpOptions.path = `/voice/v1/call/${callVoiceId}/connect`;
+  httpOptions.method = 'PUT';
+
+  const postData = JSON.stringify({
+  	from : fromNumber,
+	to : toNumber
+  });
+
+  connectEnablexServer(postData, (response) => {
+    logger.info(`RESPONSE:- ${response}`);
+    callback(response);
+  });
+}
+
+// Voice API to start the Recording
+function startRecording(callVoiceId, recordingName, callback) {
+  httpOptions.path = `/voice/v1/call/${callVoiceId}/recording`;
+  httpOptions.method = 'PUT';
+
+  const postData = JSON.stringify({
+        start : true,
+        recording_name : recordingName
+  });
+
+  connectEnablexServer(postData, (response) => {
+    logger.info(`RESPONSE:- ${response}`);
+    callback(response);
+  });
+}
+
+// Voice API to stop the Recording
+function stopRecording(callVoiceId, callback) {
+  httpOptions.path = `/voice/v1/call/${callVoiceId}/recording`;
+  httpOptions.method = 'PUT';
+
+  const postData = JSON.stringify({
+        stop : true,
+  });
+
+  connectEnablexServer(postData, (response) => {
+    logger.info(`RESPONSE:- ${response}`);
+    callback(response);
+  });
+}
+
+// Voice API to fetch the Recording
+function fetchRecording(recordingId , serviceid, callback) {
+  httpOptions.path = `/voice/v1/recording/${serviceId}/${recordingId}`;
+  httpOptions.method = 'GET';
+
+  connectEnablexServer(postData, (response) => {
+    logger.info(`RESPONSE:- ${response}`);
+    callback(response);
+  });
+}
+
+
+// Voice API to forward the call
+function forwardCall(callVoiceId , fromNumber , toNumber, callback) {
+  httpOptions.path = `/voice/v1/call/${callVoiceId}/forward`;
+  httpOptions.method = 'PUT';
+
+  const postData = JSON.stringify({
+	  from : fromNumber,
+	  to : toNumber
+  });
+
+  connectEnablexServer(postData, (response) => {
+    logger.info(`RESPONSE:- ${response}`);
+    callback(response);
+  });
+}
+
+// Voice API to stop the Recording
+function holdCall(callVoiceId, callback) {
+  httpOptions.path = `/voice/v1/call/${callVoiceId}/hold`;
+  httpOptions.method = 'PUT';
+
+  const postData = JSON.stringify({
+        hold : true,
+  });
+
+  connectEnablexServer(postData, (response) => {
+    logger.info(`RESPONSE:- ${response}`);
+    callback(response);
+  });
+}
+
+// Voice API to stop the Recording
+function resumeCall(callVoiceId, callback) {
+  httpOptions.path = `/voice/v1/call/${callVoiceId}/hold`;
+  httpOptions.method = 'PUT';
+
+  const postData = JSON.stringify({
+        hold : false,
+  });
+
+  connectEnablexServer(postData, (response) => {
+    logger.info(`RESPONSE:- ${response}`);
+    callback(response);
+  });
+}
+
+// Voice API to stop the Recording
+function transferCall(callVoiceId, fromNumber, toNumber, callback) {
+  httpOptions.path = `/voice/v1/call/${callVoiceId}/transfer`;
+  httpOptions.method = 'PUT';
+
+  const postData = JSON.stringify({
+	  from : fromNumber,
+	  transfer_to : toNumber
+  });
+
+  connectEnablexServer(postData, (response) => {
+    logger.info(`RESPONSE:- ${response}`);
+    callback(response);
+  });
+}
+
+//Create an Outbound Call and join room
+function makeOutboundCallandJoinRoom(reqDetails, calllback) {
+	  httpOptions.path = `/voice/v1/room/${reqDetails.room_id}/join`;
+  httpOptions.method = 'POST';
+  const postData = JSON.stringify({
+    name: 'TEST_APP',
+    owner_ref: 'XYZ',
+    to: reqDetails.to,
+    from: reqDetails.from,
+    event_url: `${process.env.PUBLIC_WEBHOOK_URL}/event`,
+  });
+
+  connectEnablexServer(postData, (response) => {
+    logger.info(`RESPONSE:- ${response}`);
+    callback(response);
+  });
+}
+
+//Join Inbound/Outbound Voice Call to room
+function JoinRoom(callVoiceId, roomId , calllback) {
+  httpOptions.path = `/voice/v1/room/${roomId}/call/${callVoiceId}/join`;
+  httpOptions.method = 'PUT';
+
+  connectEnablexServer('', (response) => {
+    logger.info(`RESPONSE:- ${response}`);
+    callback(response);
+  });
+}
+
+//Create Outbound Conference Call
+function OutboundCreateConference(reqDetails, calllback) {
+  httpOptions.path = `/voice/v1/conference`;
+  httpOptions.method = 'POST';
+  const postData = JSON.stringify({
+    name: 'TEST_APP',
+    owner_ref: 'XYZ',
+    to: reqDetails.to,
+    from: reqDetails.from,
+    event_url: `${process.env.PUBLIC_WEBHOOK_URL}/event`,
+  });
+
+  connectEnablexServer(postData, (response) => {
+    logger.info(`RESPONSE:- ${response}`);
+    callback(response);
+  });
+}
+
+//make Outbound Call and join existing conference.
+function OuboundJoinConference(fromNumber , toNumber , conferenceId, callback) {
+  httpOptions.path = `/voice/v1/conference/${conferencId}`;
+  httpOptions.method = 'POST';
+  const postData = JSON.stringify({
+    from : fromNumber,
+    to : toNumber,
+  });
+
+  connectEnablexServer(postData, (response) => {
+    logger.info(`RESPONSE:- ${response}`);
+    callback(response);
+  });
+}
+
 // Voice API call to hangup the call
 function hangupCall(callVoiceId, callback) {
   httpOptions.path = `/voice/v1/call/${callVoiceId}`;
